a644baac2e19ee1951dd775d3ab1cec5ba77bb64
Dernbu <63487502+Dernbu@users.noreply.github.com>
2022-08-14 03:22:08 +0800

    timer styling

--- components/timer.tsx
+++ components/timer.tsx
@@ -2,9 +2,11 @@ import { useEffect } from "react";
 import { useTimer } from "../libraries/hooks";
 import { seconds } from "../libraries/utils/date-utils";
 
+import styles from "./timer.module.css";
+
 export function Timer({ initialDuration, onFinish }) {
     const [timeLeft] = useTimer(initialDuration);
-    
+
     useEffect(() => {
         if (seconds(timeLeft) === 0) {
             onFinish();
@@ -12,8 +14,15 @@ export function Timer({ initialDuration, onFinish }) {
     }, [timeLeft]);
 
     return (
-        <div>
-            {pad_digit(timeLeft.hours)}:{pad_digit(timeLeft.minutes)}:{pad_digit(timeLeft.seconds)}
+        <div className={styles.timer}>
+            <div className={styles.timerText}>
+                {pad_digit(timeLeft.hours)}:{pad_digit(timeLeft.minutes)}:{pad_digit(timeLeft.seconds)}
+            </div>
+            {/* <hr /> */}
+            <div className={styles.timerButtonLayout}>
+                <button className={styles.timerButton}>Pause</button>
+                <button className={styles.timerButton}>Stop</button>
+            </div>
         </div>
     )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,5 @@
+
+import 'normalize.css/normalize.css';
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
